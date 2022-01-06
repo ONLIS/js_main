ee624977864ba9f1a733b21f50ef0dbb460faea3
onlis <aleksei@onl.is>
2022-01-06 21:21:16 +0500

    lesson 27 done

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import validate from "./modules/validate";
 import tabs from "./modules/tabs";
 import slider from "./modules/slider";
 import calc from "./modules/calc";
+import sendForm from "./modules/sendForm";
+
+const forms = document.querySelectorAll("form");
 
 timer("01 january 2022");
 menu();
@@ -13,3 +16,7 @@ validate();
 tabs();
 slider();
 calc(100);
+
+forms.forEach((form) => {
+  sendForm({ formId: form.id, someElem: [{ type: "block", id: "total" }] });
+});
